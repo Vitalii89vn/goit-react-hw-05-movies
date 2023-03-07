f6306f34975d5174583b0ca15fd466d6e7b0bafc
Vitalii89vn <vitalii.kuhta@gmail.com>
2023-03-07 17:54:17 +0200

    check

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -3,7 +3,7 @@ import { SearchBar } from "components/Searchbar/Searchbar";
 import { Outlet, useSearchParams  } from "react-router-dom";
 import { useState, useEffect } from "react";
 import * as FetchTMDB from "utils/FetchTMDB";
-import { toast } from "react-hot-toast";
+import toast from 'react-hot-toast';
 import { MoviesList } from "components/MoviesList/MoviesList";
 import { Loader } from "components/Loader/Loader";
 
@@ -14,22 +14,23 @@ export const Movies = () => {
      const [loading, setLoading] = useState(false);
 
     let currentQuery = searchParams.get('query');
-     if (currentQuery) { searchQuery = currentQuery };
+    let currQuery = (currentQuery && searchQuery === '') ? currentQuery : searchQuery;
+ 
     useEffect(() => {
-        if (searchQuery === '') return;
-
+        if (!currQuery) return;
+  
         setLoading(true);
-        FetchTMDB.searchMovies(searchQuery)
+        FetchTMDB.searchMovies(currQuery)
             .then(m => {
                 if (m.total_results !== 0) {
+                    console.log(m);
                     setMovies(m);
-                    setSearchParams({ query: searchQuery })
-                }
-                else { toast.error('OOPS') }
+                    setSearchParams({ query: currQuery });
+                } else { toast.error('This is an error!') };
             })
-        .catch(() => toast.error('OOPS'))
+        .catch(() => toast('OOPS'))
         .finally(() => setLoading(false))
-}, [searchQuery, setSearchParams]);
+}, [setSearchParams, currQuery, searchQuery]);
 
  function handleFormSubmit(searchQuery) {
    setSearchQuery(searchQuery);
